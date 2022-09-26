6bd51cb9241e07d3e1f43b5b1087e43e5afbeada
asurgingriver <aSurgingRiver@163.com>
2022-09-26 21:26:18 +0800

    5.0
    增加命令行工具

--- Source/WebView/Private/WebView.cpp
+++ Source/WebView/Private/WebView.cpp
@@ -19,7 +19,6 @@ private:
 
 void FWebViewModule::ShutdownModule()
 {
-	UE_LOG(LogTemp, Log, TEXT("WebView ++++++++++++++++++++++++++++++++"));
 	// This function may be called during shutdown to clean up your module.  
 	// For modules that support dynamic reloading,
 	// we call this function before unloading the module.
