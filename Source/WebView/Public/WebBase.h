d404d0da7e5d1ca5aeb46be3971cbb874898779b
asurgingriver <aSurgingRiver@163.com>
2023-06-12 21:23:09 +0800

    5.1

--- Source/WebView/Public/WebBase.h
+++ Source/WebView/Public/WebBase.h
@@ -94,8 +94,11 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Initial URL"), Category = "Web View")
 		FString urlInitial;
 	/** Configure webpage  mouse is transparency */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Enable Transparency"), Category = "Web View")
-		bool bEnableTransparency = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Enable Mouse"), Category = "Web View|Transparency")
+		bool bEnableMouseTransparency = true;
+	/** Configure webpage  mouse is transparency */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Keyborad Mode"), Category = "Web View|Transparency")
+		WebView_Keyboard_Mode eKeyboradModeTransparency ;
 	/** Control and Editor show text style  */
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "Text Style", UIMin = 0, UIMax = 1), Category = "Web View|Show Head")
 		FTextBlockStyle  styleText;
@@ -251,6 +254,11 @@ public:
 	*/
 	UFUNCTION(BlueprintCallable, Category = "Web View")
 	void ShowDevTools();
+	/**
+	* Show Dev Tools for debug web
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Web View")
+	void KeyboardMode(WebView_Keyboard_Mode KeyMode);
 public:
 	virtual void BeginDestroy() override;
 	// 
